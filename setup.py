c0efea5367344fce9f2b9508c80cddd17d091c02
Lukas <Lukas.Knosp@student.uibk.ac.at>
2017-01-19 22:24:43 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='plotting_routine',
+      version='0.1.alpha',
+      description='Quick function plotter',
+      author='Lukas Knosp',
+      author_email='Lukas.Knosp@student.uibk.ac.at',
+      url='https://github.com/csaq5147/plotting_routine.git',
+      scripts=['miracle']
+     )
